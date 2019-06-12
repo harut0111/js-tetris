bab9dc57fef9b19495db05856e0d9a64405eef7e
Harut Muqoyan <hmuqoyan@gmail.com>
2019-06-12 20:14:02 +0400

    added tetrominoes.js file

--- js/tetris.js
+++ js/tetris.js
@@ -30,11 +30,36 @@ for(let r = 0; r < ROW; r++) {
 function drawBoard() {
     for (let r = 0; r < ROW; r++) {
         for (let c = 0; c < COLUMN; c++) {
-            drewSquare(c, r, board[r][c]); 
-            
-        }
-        
+            drewSquare(c, r, board[r][c]);  
+        }    
     }
 }
-
 drawBoard();
+
+//////
+const Z = [
+    [[1,1,0], [0,1,1], [0,0,0]]
+]
+
+let piece = Z[0];
+const pieceColor = 'orange';
+for (let r = 0; r < piece.length; r++) {
+    for (let c = 0; c < piece.length; c++) {
+        if (piece[r][c]) {
+            drewSquare(c,r,pieceColor);
+        }
+    } 
+}
+
+//Piece Class
+
+class Piece {
+    constructor(tetromino, color) {
+        this.tetromino = tetromino;
+        this.tetrominoN = 0;
+        this.activeTetromino = this.tetromino[this.tetrominoN];
+        this.color = color;
+        this.x = 3;
+        this.y = -2;
+    }
+}
